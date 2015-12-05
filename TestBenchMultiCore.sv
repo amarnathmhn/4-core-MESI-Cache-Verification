649e1a8f455af474f1665d2627e4514c52f88f59
Amarnath <amarnathmhn@gmail.com>
2015-12-04 17:01:24 -0800

    connections made

--- TestBenchMultiCore.sv
+++ TestBenchMultiCore.sv
@@ -7,7 +7,7 @@
 //include Design Files
 `include "cache_multi_config_1.v" //DUT design file 
 `include "arbiter.v"
-`include "TestCases.sv"
+//`include "TestCases.sv"
 //define half clock period
 `define HALF_PERIOD 100
 
@@ -16,56 +16,170 @@ module top_C1();
 
  //Global interface containing all the signals that need to be
  //driven/monitored
- globalInterface g_intf #(.cores(4))(.clk(a.clk));
+ globalInterface g_intf [8](.clk(a.clk));
 
 
  //Virtual interface for global interface
  virtual interface globalInterface local_intf;
  //Connect internal registers of DUT to interface
- assign g_intf.Cache_var            = P1_DL.cb.Cache_var;
- assign g_intf.Cache_proc_contr     = P1_DL.cb.Cache_proc_contr;
- assign g_intf.LRU_var              = P1_DL.cc.LRU_var;
- assign g_intf.LRU_replacement_proc = P1_DL.cc.LRU_replacement_proc;
+ assign g_intf[0].Cache_var            = CMC.P1_DL.cb.Cache_var;
+ assign g_intf[0].Cache_proc_contr     = CMC.P1_DL.cb.Cache_proc_contr;
+ assign g_intf[0].LRU_var              = CMC.P1_DL.cc.LRU_var;
+ assign g_intf[0].LRU_replacement_proc = CMC.P1_DL.cc.LRU_replacement_proc;
+
+ assign g_intf[1].Cache_var            = CMC.P2_DL.cb.Cache_var;
+ assign g_intf[1].Cache_proc_contr     = CMC.P2_DL.cb.Cache_proc_contr;
+ assign g_intf[1].LRU_var              = CMC.P2_DL.cc.LRU_var;
+ assign g_intf[1].LRU_replacement_proc = CMC.P2_DL.cc.LRU_replacement_proc;
+
+
+ assign g_intf[2].Cache_var            = CMC.P3_DL.cb.Cache_var;
+ assign g_intf[2].Cache_proc_contr     = CMC.P3_DL.cb.Cache_proc_contr;
+ assign g_intf[2].LRU_var              = CMC.P3_DL.cc.LRU_var;
+ assign g_intf[2].LRU_replacement_proc = CMC.P3_DL.cc.LRU_replacement_proc;
+
+ assign g_intf[3].Cache_var            = CMC.P4_DL.cb.Cache_var;
+ assign g_intf[3].Cache_proc_contr     = CMC.P4_DL.cb.Cache_proc_contr;
+ assign g_intf[3].LRU_var              = CMC.P4_DL.cc.LRU_var;
+ assign g_intf[3].LRU_replacement_proc = CMC.P4_DL.cc.LRU_replacement_proc;
+
+ assign g_intf[4].Cache_var            = CMC.P5_DL.cb.Cache_var;
+ assign g_intf[4].Cache_proc_contr     = CMC.P5_DL.cb.Cache_proc_contr;
+ assign g_intf[4].LRU_var              = CMC.P5_DL.cc.LRU_var;
+ assign g_intf[4].LRU_replacement_proc = CMC.P5_DL.cc.LRU_replacement_proc;
+
+
+ assign g_intf[5].Cache_var            = CMC.P6_DL.cb.Cache_var;
+ assign g_intf[5].Cache_proc_contr     = CMC.P6_DL.cb.Cache_proc_contr;
+ assign g_intf[5].LRU_var              = CMC.P6_DL.cc.LRU_var;
+ assign g_intf[5].LRU_replacement_proc = CMC.P6_DL.cc.LRU_replacement_proc;
+
+ assign g_intf[6].Cache_var            = CMC.P7_DL.cb.Cache_var;
+ assign g_intf[6].Cache_proc_contr     = CMC.P7_DL.cb.Cache_proc_contr;
+ assign g_intf[6].LRU_var              = CMC.P7_DL.cc.LRU_var;
+ assign g_intf[6].LRU_replacement_proc = CMC.P7_DL.cc.LRU_replacement_proc;
+
+
+ assign g_intf[7].Cache_var            = CMC.P8_DL.cb.Cache_var;
+ assign g_intf[7].Cache_proc_contr     = CMC.P8_DL.cb.Cache_proc_contr;
+ assign g_intf[7].LRU_var              = CMC.P8_DL.cc.LRU_var;
+ assign g_intf[7].LRU_replacement_proc = CMC.P8_DL.cc.LRU_replacement_proc;
+
  always @(g_intf.clk) begin
-   g_intf.Updated_MESI_state_proc  = P1_DL.cb.Updated_MESI_state_proc; 
-   g_intf.Blk_access_proc          = P1_DL.cb.Blk_access_proc;
-   g_intf.Blk_access_snoop         = P1_DL.cb.Blk_access_snoop;
-   g_intf.Index_snoop              = P1_DL.cb.Index_snoop;
-    
+   g_intf[0].Updated_MESI_state_proc  = P1_DL.cb.Updated_MESI_state_proc; 
+   g_intf[0].Blk_access_proc          = P1_DL.cb.Blk_access_proc;
+   g_intf[0].Blk_access_snoop         = P1_DL.cb.Blk_access_snoop;
+   g_intf[0].Index_snoop              = P1_DL.cb.Index_snoop;
+   
+   g_intf[1].Updated_MESI_state_proc  = P2_DL.cb.Updated_MESI_state_proc; 
+   g_intf[1].Blk_access_proc          = P2_DL.cb.Blk_access_proc;
+   g_intf[1].Blk_access_snoop         = P2_DL.cb.Blk_access_snoop;
+   g_intf[1].Index_snoop              = P2_DL.cb.Index_snoop;
+
+   g_intf[2].Updated_MESI_state_proc  = P3_DL.cb.Updated_MESI_state_proc; 
+   g_intf[2].Blk_access_proc          = P3_DL.cb.Blk_access_proc;
+   g_intf[2].Blk_access_snoop         = P3_DL.cb.Blk_access_snoop;
+   g_intf[2].Index_snoop              = P3_DL.cb.Index_snoop;
+
+   g_intf[3].Updated_MESI_state_proc  = P4_DL.cb.Updated_MESI_state_proc; 
+   g_intf[3].Blk_access_proc          = P4_DL.cb.Blk_access_proc;
+   g_intf[3].Blk_access_snoop         = P4_DL.cb.Blk_access_snoop;
+   g_intf[3].Index_snoop              = P4_DL.cb.Index_snoop;
+
+   g_intf[4].Updated_MESI_state_proc  = P5_DL.cb.Updated_MESI_state_proc; 
+   g_intf[4].Blk_access_proc          = P5_DL.cb.Blk_access_proc;
+   g_intf[4].Blk_access_snoop         = P5_DL.cb.Blk_access_snoop;
+   g_intf[4].Index_snoop              = P5_DL.cb.Index_snoop;
+
+   g_intf[5].Updated_MESI_state_proc  = P6_DL.cb.Updated_MESI_state_proc; 
+   g_intf[5].Blk_access_proc          = P6_DL.cb.Blk_access_proc;
+   g_intf[5].Blk_access_snoop         = P6_DL.cb.Blk_access_snoop;
+   g_intf[5].Index_snoop              = P6_DL.cb.Index_snoop;
+
+   g_intf[6].Updated_MESI_state_proc  = P7_DL.cb.Updated_MESI_state_proc; 
+   g_intf[6].Blk_access_proc          = P7_DL.cb.Blk_access_proc;
+   g_intf[6].Blk_access_snoop         = P7_DL.cb.Blk_access_snoop;
+   g_intf[6].Index_snoop              = P7_DL.cb.Index_snoop;
+
+   g_intf[7].Updated_MESI_state_proc  = P8_DL.cb.Updated_MESI_state_proc; 
+   g_intf[7].Blk_access_proc          = P8_DL.cb.Blk_access_proc;
+   g_intf[7].Blk_access_snoop         = P8_DL.cb.Blk_access_snoop;
+   g_intf[7].Index_snoop              = P8_DL.cb.Index_snoop; 
+  
  end
 //The port connections are made without the knowledge of the actual design. It is supposed to change later.
 cache_multi_config_1 CMC (
-                        g_intf.clk,
-                        g_intf.PrWr[0],
-                        g_intf.PrWr[1],
-                        g_intf.PrWr[2],
-                        g_intf.PrWr[3],
-                        g_intf.PrRd[0],
-                        g_intf.PrRd[1],
-                        g_intf.PrRd[2],
-                        g_intf.PrRd[3],
-                        g_intf.Address[0],
-                        g_intf.Address[1],
-                        g_intf.Address[2],
-                        g_intf.Address[3],
-                        g_intf.Data_Bus[0],
-                        g_intf.Data_Bus[1],
-                        g_intf.Data_Bus[2],
-                        g_intf.Data_Bus[3],
-                        g_intf.CPU_stall[0],
-                        g_intf.CPU_stall[1],
-                        g_intf.CPU_stall[2],
-                        g_intf.CPU_stall[3],
-                        g_intf.Com_Bus_Req_proc_0,
-                        g_intf.Com_Bus_Gnt_proc_0,
-                        g_intf.Com_Bus_Req_snoop_0,
-                        g_intf.Com_Bus_Gnt_snoop_0,
-                        g_intf.Address_Com,
-                        g_intf.Data_Bus_Com,
-                        g_intf.Data_in_Bus,
-                        g_intf.Mem_wr,
-                        g_intf.Mem_oprn_abort,
-                        g_intf.Mem_write_done,
+                                                                g_intf.clk,
+								g_intf[0].PrWr              ,
+								g_intf[0].PrRd              ,
+								g_intf[0].Address           ,
+								g_intf[0].Data_Bus          ,
+								g_intf[0].CPU_stall         ,
+								g_intf[0].Com_Bus_Req_proc_0,
+								g_intf[0].Com_Bus_Gnt_proc_0,
+								g_intf[0].Com_Bus_Req_snoo_0,
+								g_intf[0].Com_Bus_Gnt_snoop_0,
+								g_intf[1].PrWr              ,
+								g_intf[1].PrRd              ,
+								g_intf[1].Address           ,
+								g_intf[1].Data_Bus          ,
+								g_intf[1].CPU_stall         ,
+								g_intf[1].Com_Bus_Req_proc  ,
+								g_intf[1].Com_Bus_Gnt_proc  ,
+								g_intf[1].Com_Bus_Req_snoop ,
+								g_intf[1].Com_Bus_Gnt_snoop ,
+								g_intf[2].PrWr              ,
+								g_intf[2].PrRd              ,
+								g_intf[2].Address           ,
+								g_intf[2].Data_Bus          ,
+								g_intf[2].CPU_stall         ,
+								g_intf[2].Com_Bus_Req_proc  ,
+								g_intf[2].Com_Bus_Gnt_proc  ,
+								g_intf[2].Com_Bus_Req_snoop ,
+								g_intf[2].Com_Bus_Gnt_snoop ,
+								g_intf[3].PrWr              ,
+								g_intf[3].PrRd              ,
+								g_intf[3].Address           ,
+								g_intf[3].Data_Bus          ,
+								g_intf[3].CPU_stall         ,
+								g_intf[3].Com_Bus_Req_proc  ,
+								g_intf[3].Com_Bus_Gnt_proc  ,
+								g_intf[3].Com_Bus_Req_snoop ,
+								g_intf[3].Com_Bus_Gnt_snoop ,
+								g_intf[0].Address_Com,
+								g_intf[0].Data_Bus_Com,
+								g_intf[0].Data_in_Bus,
+								g_intf[0].Mem_wr,
+								g_intf[0].Mem_oprn_abort,
+								g_intf[0].Mem_write_done,
+								g_intf[4].PrWr              ,
+								g_intf[4].PrRd              ,
+								g_intf[4].Address           ,
+								g_intf[4].Data_Bus          ,
+								g_intf[4].CPU_stall         ,
+								g_intf[4].Com_Bus_Req_proc  ,
+								g_intf[4].Com_Bus_Gnt_proc  ,
+								g_intf[5].PrWr              ,
+								g_intf[5].PrRd              ,
+								g_intf[5].Address           ,
+								g_intf[5].Data_Bus          ,
+								g_intf[5].CPU_stall         ,
+								g_intf[5].Com_Bus_Req_proc  ,
+								g_intf[5].Com_Bus_Gnt_proc  ,
+								g_intf[6].PrWr              ,
+								g_intf[6].PrRd              ,
+								g_intf[6].Address           ,
+								g_intf[6].Data_Bus          ,
+								g_intf[6].CPU_stall         ,
+								g_intf[6].Com_Bus_Req_proc  ,
+								g_intf[6].Com_Bus_Gnt_proc  ,
+								g_intf[7].PrWr              ,
+								g_intf[7].PrRd              ,
+								g_intf[7].Address           ,
+								g_intf[7].Data_Bus          ,
+								g_intf[7].CPU_stall         ,
+								g_intf[7].Com_Bus_Req_proc  ,
+								g_intf[7].Com_Bus_Gnt_proc  
 
 );
  
@@ -98,7 +212,7 @@ arbiter a (
 			g_intf.Mem_snoop_gnt
 );
 
-//Instantiate  Top level direct testcase object. Please consider that these
+/*//Instantiate  Top level direct testcase object. Please consider that these
 //test cases consider more than 1 scenario specified in Test Plan and As
 //commented in TestCases.sv file
 topReadMiss                     topReadMiss_inst;
@@ -305,4 +419,6 @@ $display("Testing Read Miss Scenario using topReadMiss test case when other Cach
 always @(posedge g_intf.clk)
 
    g_intf.check_UndefinedBehavior(0);
+
+*/
 endmodule
